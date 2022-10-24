840da4f5fa59cfa518ec5df30e238e029febdeea
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2022-10-24 11:30:56 +0100

    Create MenuControl.cs

---
+++ Game 1/MenuControl.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    public string LevelOne;
+    public string DigitSpan;
+    public int DigitSpanAttempts;
+    public string SpatialSpan;
+
+    public Text DigitSpanBestText;
+    public Text DigitSpanAttemptsText;
+
+    public int SpatialSpanAttempts;
+    public Text SpatialSpanBestText;
+    public Text SpatialSpanAttemptsText;
+
+    public GameObject MaxAttempts;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Spatial Span
+        SpatialSpanAttempts = PlayerPrefs.GetInt("SpatialSpanAttempts");
+
+        SpatialSpanAttemptsText.text = "Attempts: " + PlayerPrefs.GetInt("SpatialSpanAttempts", DigitSpanAttempts).ToString();
+
+
+        SpatialSpanBestText.text = "Best Level: " + PlayerPrefs.GetInt("Spatial1BestLevel", 0).ToString();
+
+        //Digit Span
+
+        DigitSpanAttempts = PlayerPrefs.GetInt("DigitSpanAttempts"); 
+
+        DigitSpanAttemptsText.text = "Attempts: " + PlayerPrefs.GetInt("DigitSpanAttempts", DigitSpanAttempts).ToString();
+
+
+        DigitSpanBestText.text = "Best Level: " + PlayerPrefs.GetInt("Digit1BestLevel", 0).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            Reset();
+        }
+
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(LevelOne);
+        Debug.Log("Level One Begins");
+    }
+
+
+    public void StartDigitSpan() 
+     {
+     //if the user attempts to play the digit span mode and they have played 5 times, inform them that they have reached their max attempts. else start the digit span mode
+        if (DigitSpanAttempts = 5)
+        {
+            MaxAttempts.SetActive(true);
+        }
+        else
+        {
+            DigitSpanAttempts = DigitSpanAttempts + 1;
+
+            PlayerPrefs.SetInt("DigitSpanAttempts", DigitSpanAttempts);
+
+            SceneManager.LoadSceneAsync(DigitSpan);
+            Debug.Log("Digit Span Begins");
+        }
+        
+
+        
+    }
+
+    public void StartSpatialSpan()
+    {
+    //if the user attempts to play the spatial span mode and they have played 5 times, inform them that they have reached their max attempts. else start the spatial span mode
+        if (SpatialSpanAttempts > 4)
+        {
+            MaxAttempts.SetActive(true);
+        }
+        else
+        {
+            SpatialSpanAttempts = SpatialSpanAttempts + 1;
+
+            PlayerPrefs.SetInt("SpatialSpanAttempts", SpatialSpanAttempts);
+
+            SceneManager.LoadSceneAsync(SpatialSpan);
+            Debug.Log("Spatial Span Begins");
+        }
+    }
+
+    public void QuitGame() //Closes the application
+    {
+        Application.Quit();
+        Debug.Log("Application Closed Succesfully");
+
+    }
+
+    public void Reset() //Resets number of attempts
+    {
+        PlayerPrefs.DeleteAll();
+        DigitSpanAttemptsText.text = "Attempts: " + PlayerPrefs.GetInt("DigitSpanAttempts", DigitSpanAttempts).ToString();
+        DigitSpanBestText.text = "Best Level: " + PlayerPrefs.GetInt("Digit1BestLevel", 0).ToString();
+
+        SpatialSpanAttemptsText.text = "Attempts: " + PlayerPrefs.GetInt("SpatialSpanAttempts", DigitSpanAttempts).ToString();
+        SpatialSpanBestText.text = "Best Level: " + PlayerPrefs.GetInt("Spatial1BestLevel", 0).ToString();
+    }
+
+}
